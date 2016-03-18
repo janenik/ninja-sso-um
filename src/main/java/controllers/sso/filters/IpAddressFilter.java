2497dd66a71161f7381719eaadf8186084330080
janenik <jane.nikuliak@yahoo.com>
2016-03-17 18:30:25 -0700

    Fixing IP filter, moving proxy IP header to configuration and updating Ninja version to 5.4.0

--- src/main/java/controllers/sso/filters/IpAddressFilter.java
+++ src/main/java/controllers/sso/filters/IpAddressFilter.java
@@ -1,12 +1,16 @@
 package controllers.sso.filters;
 
+import com.google.common.collect.Sets;
 import ninja.Context;
 import ninja.Filter;
 import ninja.FilterChain;
 import ninja.Result;
+import ninja.utils.NinjaProperties;
 import org.slf4j.Logger;
 
 import javax.inject.Inject;
+import java.util.Collections;
+import java.util.Set;
 
 /**
  * IP address filter: extracts IP address from request or headers (for example, provided by nginx front server).
@@ -19,45 +23,68 @@ public class IpAddressFilter implements Filter {
     public static final String REMOTE_IP = "REMOTE_IP";
 
     /**
-     * Header name to extract IP address from. Used when remote address from request context is undefined or localhost.
+     * Default header name to extract IP address from. Used when remote address from request context is undefined or
+     * localhost.
      */
-    private static final String IP_HEADER_NAME = "x-real-ip";
+    private static final String PROXY_PASS_IP_HEADER_NAME = "X-Real-IP";
 
     /**
-     * Default IP remote address, v4.
+     * Localhost address, v4.
      */
-    private static final String DEFAULT_IP = "127.0.0.1";
+    private static final String LOCALHOST_IPV4 = "127.0.0.1";
 
     /**
-     * Default IP remote address, v6.
+     * Localhost address, v6.
      */
-    private static final String DEFAULT_IP_V6 = "::1";
+    private static final String LOCALHOST_IPV6 = "::1";
 
     /**
-     * Default IP remote address, v6 with subnet.
+     * Set that contains possible representations of localhost (IPv4 and IPv6).
      */
-    private static final String DEFAULT_IP_V6_W_SUBNET = "::1/128";
-
-    /**
-     * Default full IP remote address, v6.
-     */
-    private static final String DEFAULT_IP_V6_FULL = "0000:0000:0000:0000:0000:0000:0000:0001";
+    private static final Set<String> LOCALHOSTS = Collections.unmodifiableSet(
+            Sets.newHashSet(LOCALHOST_IPV6, LOCALHOST_IPV4, "localhost", "::1/128",
+                    "0000:0000:0000:0000:0000:0000:0000:0001", "0:0:0:0:0:0:0:1"));
 
     /**
      * Logger.
      */
+    final Logger logger;
+
+    /**
+     * Ninja properties.
+     */
+    final NinjaProperties properties;
+
+    /**
+     * Proxy IP header name. Value provided in header by frontend server like nginx.
+     */
+    final String proxyIpHeaderName;
+
+    /**
+     * Proxy IP header name, lowercased. Value provided in header by frontend server like nginx.
+     */
+    final String proxyIpHeaderNameLowerCased;
+
     @Inject
-    Logger logger;
+    public IpAddressFilter(NinjaProperties properties, Logger logger) {
+        this.properties = properties;
+        this.logger = logger;
+
+        this.proxyIpHeaderName = properties.getWithDefault("app.proxy.ipHeaderName", PROXY_PASS_IP_HEADER_NAME);
+        this.proxyIpHeaderNameLowerCased = proxyIpHeaderName.toLowerCase();
+    }
 
     @Override
     public Result filter(FilterChain filterChain, Context context) {
-        String ip = context.getRemoteAddr();
-        if (ip == null || ip.isEmpty() || DEFAULT_IP.equals(ip) ||
-                DEFAULT_IP_V6.equals(ip) || DEFAULT_IP_V6_W_SUBNET.equals(ip) || DEFAULT_IP_V6_FULL.equals(ip)) {
-            ip = context.getHeader(IP_HEADER_NAME);
+        String ip = context.getHeader(proxyIpHeaderName);
+        if (ip == null) {
+            ip = context.getHeader(proxyIpHeaderNameLowerCased);
+        }
+        if (ip == null || ip.isEmpty() || LOCALHOSTS.contains(ip)) {
+            ip = context.getRemoteAddr();
         }
         if (ip == null) {
-            ip = DEFAULT_IP_V6;
+            ip = LOCALHOST_IPV6;
         }
         context.setAttribute(REMOTE_IP, ip);
         if (logger.isInfoEnabled()) {
