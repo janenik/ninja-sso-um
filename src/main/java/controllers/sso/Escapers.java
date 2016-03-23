aa4d68fa2eee44e487d7b6c079e0835adc14a156
janenik <jane.nikuliak@yahoo.com>
2016-03-22 22:00:37 -0700

    Adding escaper: for encoding uses Guava's escaper, for decoding - Java

---
+++ src/main/java/controllers/sso/Escapers.java
@@ -0,0 +1,51 @@
+package controllers.sso;
+
+import com.google.common.escape.Escaper;
+import com.google.common.net.PercentEscaper;
+
+import java.net.URLDecoder;
+
+/**
+ * Encoding utils.
+ */
+public final class Escapers {
+
+    /**
+     * More efficient encoding (acts like encodeURIComponent).
+     */
+    private static final Escaper percentEscaper = new PercentEscaper("", false);
+
+    /**
+     * Encodes given string characters using a UTF-8 based percent
+     * encoding scheme. See {@link com.google.common.net.PercentEscaper} documentation.
+     *
+     * @param str String to escape.
+     * @return URL encoded string.
+     */
+    public static String encodePercent(String str) {
+        if (str == null) {
+            return "";
+        }
+        return percentEscaper.escape(str);
+    }
+
+    /**
+     * Decodes given string that contains some percent encoded values.
+     *
+     * @param str String to URL decode.
+     * @return URL decode string.
+     */
+    public static String decodePercent(String str) {
+        if (str == null) {
+            return "";
+        }
+        try {
+            return URLDecoder.decode(str, "UTF-8");
+        } catch (java.io.UnsupportedEncodingException wow) {
+            throw new RuntimeException(wow.getMessage(), wow);
+        }
+    }
+
+    private Escapers() {
+    }
+}
